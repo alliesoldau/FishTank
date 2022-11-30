f13c0b36a968c05d5942f2c76358f5a229a9493d
Allie Soldau <alliesoldau@gmail.com>
2022-11-30 15:02:13 -0500

    Starting to create layout

---
+++ my-app/src/components/FishTank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FishTank() {
+    return(
+        <div className="Fish-tank">
+            <p>Fish Tank</p>
+        </div>
+    )
+}
+
+export default FishTank;
---
+++ my-app/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Form() {
+    return(
+        <div className="Form">
+            <p>Form</p>
+        </div>
+    )
+}
+
+export default Form;
